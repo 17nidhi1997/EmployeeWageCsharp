c44439c99f2fcf762582a5185c14975e81e2c885
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-24 10:44:01 +0530

    welcome message

---
+++ EmployeeWage.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace empwage
+{
+    class EmployeeWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Programme on Master Branch");
+        }
+    }
+}
