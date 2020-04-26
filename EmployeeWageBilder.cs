57c5b51bbf6a919573321869267189db0d0dbd65
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-26 08:00:15 +0530

    usecase 3: Added Part time Calculate Daily Employee Wage

--- EmployeeWageBilder.cs
+++ EmployeeWageBilder.cs
@@ -4,25 +4,32 @@ namespace empwage
 {
     class Program
     {
+        //constants
         public const int IS_PRESENT = 1;
         public const int WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //USECASE 2: Calculate Daily employee wage
+            //variables
             int empHrs;
             int salary;
-            attendance = random.Next(1, 2);
-            if (attendance == IS_PRESENT)
-            {
+             //USECASE 3: Add part time employee and wage
+            int attendance = random.Next(1, 3);
 
+            if (attendance == IS_PARTTIME)
+            {
+                empHrs = 4;
+            }
+            else if (attendance == IS_FULLTIME)
+            {
                 empHrs = 8;
-                salary = (WORKING_DAYS * empHrs);
             }
             else
             {
-                salary = 0;
+                empHrs = 0;
+
             }
-            Console.WriteLine("Employee dialy wage is :{0}", salary);
+            salary = (WORKING_DAYS * empHrs);
+            Console.WriteLine("Employee dialy wage:{0}", salary);
         }
     }
 }
