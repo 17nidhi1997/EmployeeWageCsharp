772479b18c0e25af56892e69aa6aca8192d4055d
17nidhi1997 <nidhinavya1997@gmail.com>
2020-04-28 08:44:49 +0530

    UASECASE 9: save the total wage for each company

--- EmployeeWageBilder.cs
+++ EmployeeWageBilder.cs
@@ -1,49 +1,68 @@
 using System;
 
-namespace empwage
+namespace EmployeeWage
 {
-    class Program
-    {
-        //constants
-        public const int IS_PARTTIME = 1;
-        public const int IS_FULLTIME = 2;
+    class EmployeeWageBuilder
+    {   //constants
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
 
-        public static int empHrs;
-        public static int empWage;
-        public static int montlysalary;
-        public static void calcEmployeeWageForCompany(String companyName, int empRate, int WORKING_DAYS, int MAX_HRS_IN_MONTH)
-        {
-            //variables
-            int totalWorkingDays = 0;
-            while (totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays <= WORKING_DAYS)
+            private string companyName;
+            private int empRate;
+            private int WORKING_DAYS;
+            private int MAX_WORKING_HRS;
+            public int empWage;
+        int empHrs;
+        int totalWage = 0;
+        public EmployeeWageBuilder(String companyName,int empRate,int WORKING_DAYS,int MAX_WORKING_HRS)
             {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empType = (random.Next() % 2);
-                switch (empType)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                   default : empHrs =0;
-                          break;
-                }
-            }
-            empWage = empHrs * empRate;
-            montlysalary = empWage * WORKING_DAYS;
-            Console.WriteLine("COMPANY NAME:"+ companyName + "----EMPWAGE:" +empWage+"------MONTLY WAGE:" +montlysalary);
-        }
-        static void Main(string[] args)
-        {
-            //USECASE 8:Compute Employee Wage for multiple companies
-            calcEmployeeWageForCompany("BridgeLabz", 20, 20, 100);
-            calcEmployeeWageForCompany("Wipro", 30, 10, 55);
-            calcEmployeeWageForCompany("D-Mart", 20, 30, 150);
-        }
-    }
-}
+                this.companyName = companyName;
+                this.empRate = empRate;
+                this.WORKING_DAYS = WORKING_DAYS;
+                this.MAX_WORKING_HRS = MAX_WORKING_HRS;
 
+            }
+            public void calcEmployeeWageForCompany()
+            {
+                int totalEmpHrs = 0;
+                int totalWorkingDays = 0;
+                while (totalEmpHrs <= MAX_WORKING_HRS && totalEmpHrs <= WORKING_DAYS )
+                {
+                    totalWorkingDays++;
+                    Random randam = new Random();
+                    int empType = (randam.Next() % 3);
+                    empHrs = getEmployeeHours(empType);
+                totalEmpHrs += empHrs;
+                empWage = empHrs * empRate;
+                totalWage += empWage;
+                Console.WriteLine("Day " + totalWorkingDays + " employee wage for company :" + companyName + "is = " + empWage);              
+            }
+            Console.WriteLine("Totaly employee wage for company: " +companyName+ "is = " +totalWage);
+        }
+
+            private int getEmployeeHours(int empType)
+            {
+             switch(empType)
+            {
+                case IS_FULL_TIME: empHrs = 8;
+                    break;
+                case IS_PART_TIME: empHrs = 4;
+                    break;
+                default: empHrs = 0;
+                    break;
+
+            }
+            return empHrs;
+            }
+        static void Main(string[] args)
+        {    //uasecase 9:ability to save the total wage for each company
+            EmployeeWageBuilder builder = new EmployeeWageBuilder("BridgeLabs",20,20,20);
+            builder.calcEmployeeWageForCompany();
+            Console.WriteLine("-----------------------");
+            EmployeeWageBuilder builderS = new EmployeeWageBuilder("IBM", 40, 20, 50);
+            builderS.calcEmployeeWageForCompany();
+        }
+            
+        }
+}
 
